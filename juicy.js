2a1b14422ec04d985e321da201b37e428ad40030
Richard Flinn <mriodine@gmail.com>
2018-09-12 21:29:04 -0700

    final commit. finalized my crappy if adventurous css file, reenabled my javascript file, and removed my test javascript file.

--- juicy.js
+++ juicy.js
@@ -4,22 +4,24 @@
 Prompts user to answer 5 yes/no questions about my interests, and creates alerts to tell
 them if they've answered correctly. 
 
-Prompts user to guess my age. If user guesses incorrectly, will create an alert telling them 
-if they have guessed high or low, and allows them to guess three more times.
+Generates a random number of "hacker duels" I have supposedly won between 20 and 100, then prompts
+ user to guess the number of hacker duels I've supposedly won. If user guesses incorrectly, will
+create an alert telling them if they have guessed high or low, and allows them to guess three more times.
 
-Prompts user to guess a musical genre I usually listen to. Allows the user to guess
+Prompts user to guess which weapons I am supposedly proficient with. Allows the user to guess
 up to six times. If user guesses correctly, or guesses more than six times, gives the user
-a message stating the possible correct answers.
+a message stating all possible correct answers.
 
 Records the number of correct answers and sends a final alert telling the user how 
 many questions they have guessed correctly. 
 */
 
-//Creates a counter for correct answers
+
+// Creates a counter for correct answers
 var correctCount = 0;
 console.log('guessCount is ' + correctCount);
 
-//Greeting message 
+// Greeting message 
 alert('Hey there! I\'m Rick. You don\'t know me - yet. See if you can guess a little about who I am!');
 
 var name = prompt('But first - what is YOUR name?');
@@ -27,113 +29,156 @@ console.log('User entered their name as ' + name);
 
 alert('Nice to meet you, ' + name + '. Let\'s test your guessing abilities.');
 
+// Creates array variables that dictate the questions, answers, and responses to the yes/no questions
+var questions = ['Do I like repetitive music?', 
+                'Here\'s an easy one: Do I love kitties?',
+                'Next question, ' + name + '. Do I hate pineapple on pizza?', 
+                'Am I an extraterrestrial life form?',
+                'Do I like science fiction?'
+                ];
 
-// Prompts user to guess whether I like crusts on my sandwich
-var crustPref = prompt('Do I prefer to remove the crust from a sandwich?');
-crustPref = crustPref.toUpperCase();
+var answer = ['y', 'y', 'n', 'y', 'y'];
 
-var crustResp;
+var respCorrect = ['Boots and cats and boots and cats and boots and cats and boots and cats and',
+                 'I love kitties, and they love me.',
+                 'Correct. I\'m glad you think so highly of me, ' + name + '.',
+                 'Plz don\'t tell NASA.',
+                 'Asimov is bae.',          
+                 ];
 
-console.log('User entered ' + crustPref);
+var respIncorrect = ['Utterly wrong. Looping is love.', 
+                    'What, do I look like some kind of monster?',
+                    'Wrong. Pineapple pizza is the one true pizza.',
+                    'Haha, yes, that\'s right. Wanna go make some crop circles?',
+                    'How would that even be possible?',
+                    ];
 
-if (crustPref === 'Y' || crustPref === "YES") {
-    console.log('User guessed no crust');
-    crustResp = 'Good guess, but I only did that in sixth grade. Finga sammiches don\'t count.';
+
+// Sends console prompts to check that y/n questions have answers and responses
+if (questions.length === answer.length) {
+    console.log('y/n questions have answers.');
 } else {
-    console.log('User guessed I prefer crust');
-    crustResp = 'Very astute, ' + name + '. I like my sammiches intact.';
-    correctCount ++;
+    console.log('error: unequal numbers of y/n questions and answers');
 }
 
-console.log('guessCount is ' + correctCount);
-
-alert(crustResp);
-
-// Prompts user to guess whether I like cats
-var petPref = prompt('Here\'s an easy one: Do I love kitties?');
-petPref = petPref.toUpperCase();
-
-var petResp;
-
-console.log('User entered ' + petPref);
-
-if (petPref === 'Y' || petPref === 'YES') {
-    console.log('User thinks I like kitters');
-    petResp = 'I love kitties, and they love me.';
-    correctCount ++;
+if (questions.length === respCorrect.length) {
+    console.log('y/n questions have "correct" responses.');
 } else {
-    console.log('User thinks I don\'t love kitties');
-    petResp = 'Do you think I\'m some kind of monster, ' + name + '?';
+    console.log('error: unequal numbers of y/n questions and "correct" responses');
 }
 
-console.log('guessCount is ' + correctCount);
-
-alert(petResp);
-
-// Prompts user to guess whether I like pineapple pizza
-var pizzaPref = prompt('Next question, ' + name + '. Do I hate pineapple on pizza?');
-pizzaPref = pizzaPref.toUpperCase();
-
-var pizzaResp;
-
-console.log('User entered ' + pizzaPref);
-
-if (pizzaPref === 'Y' || pizzaPref === 'YES') { 
-    console.log('User doesn\'t think I enjoy the superior pizza');
-    pizzaResp = 'I love pineapple pizza. I\'m sad you think I don\'t. It\'s the pinnacle of pizza technology.';
+if (questions.length == respIncorrect.length) {
+    console.log('y/n questions have "incorrect" responses');
 } else {
-    console.log('User guessed I liked pineapple on pizza');
-    pizzaResp = 'Correct. I\'m glad you think so highly of me, ' + name + '.';
-    correctCount ++;
+    console.log('error: unequal numbers of y/n questions and "incorrect" responses');
+}
+        
+
+// holds error messages for bad user inputs
+var badYNAnswer = 'You didn\'t answer yes or no!';
+var badIntAnswer = 'Integers only, please!';
+
+// Loop that prompts the user to answer y/n questions, records correct guesses, 
+// and sends an alert that responds to their answers.
+var userGuess = '';
+
+for (var i = 0; i < questions.length; i++) {
+    userGuess = prompt(questions[i]).charAt(0).toLowerCase();
+    console.log('user guessed ' + userGuess);
+
+    if (userGuess === answer[i]) {
+        alert(respCorrect[i]);
+        correctCount++;
+        console.log('user guessed correctly. guessCount is ' + correctCount);
+    } else if (userGuess != 'n' && userGuess != 'y') {
+        alert(badYNAnswer);
+        console.log('user gave bad y/n answer');
+    } else {
+        alert(respIncorrect[i]);
+        console.log('user guessed incorrectly');
+    }
 }
 
-console.log('guessCount is ' + correctCount);
+// Generates a random number between 20 and 100 and stores as the number of hacker duels I have supposedly won,
+// then prompts the user to guess that number until they have guessed four times or guessed correctly.
+// Gives the user an alert each time they guess telling them if their guess is high, low, or correct.
+var hackerDuels = Math.floor(Math.random() * 81) + 20;
+console.log('I have supposedly won ' + hackerDuels + ' hacker duels.');
 
-alert(pizzaResp);
+var guessCount = 0;
+var duelPrompt = 'Alright, now for the cool questions. How many hacker duels do you think I\'ve won? You have 4 tries to guess.';
+var duelResp = 'Nice try... I have defeated ' + hackerDuels + ' foolish hackers. Cross me and you\'ll be the ' + hackerDuels + 'th, ' + name + '.';
 
-//Prompts user to guess whether I skate
-var skatePref = prompt('Do I look cool enough to be a totally cool skater guy?');
-skatePref = skatePref.toUpperCase();
+while (guessCount < 4) {
+    userGuess = prompt(duelPrompt);
+    guessCount++;
+    duelPrompt = 'Ok, you have ' + (4 - guessCount) + ' more tries. How many hacker duels have I won?';
 
-var skateResp;
-
-console.log('User entered ' + skatePref);
-
-if (skatePref === 'Y' || skatePref === 'YES') { 
-    console.log('User guessed I\'m cool');
-    pizzaResp = 'Correct,';
-    correctCount ++;
-} else {
-    console.log('User thinks I don\'t board');
-    pizzaResp = 'Nope,';
+    if (userGuess == hackerDuels) {
+        guessCount = 4;
+        duelResp = 'That\'s right. And not a one of em stood a chance, except for Marlene... *wipes away single tear*';
+        correctCount++;
+        console.log('User guessed duels correctly');
+    } else if (userGuess > hackerDuels) {
+        alert('Wow, you must really think I\'m somethin\'... guess again.');
+        console.log('User guessed high');
+    } else if (userGuess < hackerDuels) {
+        alert('You underestimate my power!');
+        console.log('User guessed low');
+    } else if (typeof userGuess != 'number') {
+        alert('Not sure what to make of that...');
+        console.log('User may have put in some bad input.');
+    } else {
+        console.log('Not sure how we got here...');
+    }
 }
 
-console.log('guessCount is ' + correctCount);
+alert(duelResp);
 
-alert(pizzaResp + ' I do skate! Well, sorta. I can only longboard. No jump tricks, sadly. And I\'m still working on the cool part, to be honest.');
-
-// Prompts user to guess whether I believe in my professional future
-var canCode = prompt('Do I think, that I think, that I have what it takes to be a MASTER CODEWIZARD?');
-canCode = canCode.toUpperCase();
-
-var codeResp;
-
-console.log('User guessed ' + canCode);
-
-if (canCode === 'Y' || canCode === 'YES') {
-    console.log('User thinks I think can do it!');
-    codeResp = 'YES I DO! I WON\'T LET YOU DOWN!';
-    correctCount++;
-} else {
-    console.log('User doesn\'t think I believe in myself');
-    codeResp = 'HA! WRONG! I BELIEVE IN MYSEEEEELF!';
+// Prompts user to guess what science fiction / fantasy weapons I am proficient with. 
+// User answers will be compared with an array containing the weapons I can use effectively.
+// When a user guesses correctly, they will be congratulated; if not, they will be asked five more 
+// times to guess correctly. 
+var wepProfs = ['batlith', 'crysknife', 'phaser', 'nuclear icbm', 'reason', 'mediocre joke'];
+guessCount = 0;
+var guessRight = false;
+var wepPrompt = 'Alright, last question. I am proficient with a number of weapons. Try and guess one of them; you have six tries.';
+var wepResp = 'Okay, stop, I\'ll just tell you.';
+var wepAll = 'I have mastered the Batlith, crysknife, phaser, nuclear ICBM, Reason (anyone read snow crash?), and mediocre humor.';
+while (guessCount < 6 && !guessRight) {
+    userGuess = prompt(wepPrompt).toLowerCase();
+    console.log('user guessed ' + userGuess);
+    guessCount++;
+    wepPrompt = 'Still working on that one... You have ' + (6 - guessCount) + ' tries left.';
+    for (var i = 0; i < wepProfs.length; i++) {
+        switch (wepProfs[i]) {
+            case userGuess:
+                guessRight = true;
+                wepResp = 'Nice! I see you too are a weaponmaster of taste.';
+                console.log('user guessed a weapon right');
+                correctCount++;
+                break;
+            default:
+                console.log('user didn\'t get it.');
+        }
+    }
 }
 
-alert(codeResp);
-
-console.log('guessCount is ' + correctCount);
+alert(wepResp + ' ' + wepAll);
 
 
+// Gives user an alert telling them how many guesses they got right
+var knowMe = '';
 
+if (correctCount === 7) {
+    knowMe = ' NICE! NICE! YOU ARE ABSOLUTE PERFECTION!';
+    console.log('user clearly looked at my source code');
+} else if (correctCount >= 4) {
+    knowMe = ' It\'s almost like these questions are primarily based on ancient cheesy jokes.';
+    console.log('user is alright.');
+} else {
+    knowMe = ' Weak. Just... weak. It won\'t be long before you are devoured by a stray raccoon or something.';
+    console.log('user is a weakling who guessed less than three correctly.');
+}
 
-
+alert('You guessed ' + correctCount + ' out of 7 questions right!' + knowMe);
